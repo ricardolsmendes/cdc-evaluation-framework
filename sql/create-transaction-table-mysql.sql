e836165ff8a506c8be8e8bda59753958f168008e
Ricardo Mendes <ricardolsmendes@gmail.com>
2022-04-07 20:40:00 -0300

    fix: remove unnecessary chars

--- sql/create-transaction-table-mysql.sql
+++ sql/create-transaction-table-mysql.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS `transactions` (
-  `transaction_id` BIGINT NOT NULL AUTO_INCREMENT,
-  `invoice` VARCHAR(55) NOT NULL,
-  `stock_code` VARCHAR(55) NOT NULL,
-  `description` VARCHAR(255),
-  `quantity` DECIMAL(9,3) NOT NULL,
-  `invoice_date` DATETIME NOT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
-  `customer_id` DECIMAL(9,1),
-  `country` VARCHAR(255),
-  PRIMARY KEY (`transaction_id`)
+CREATE TABLE IF NOT EXISTS transactions (
+  transaction_id BIGINT NOT NULL AUTO_INCREMENT,
+  invoice VARCHAR(55) NOT NULL,
+  stock_code VARCHAR(55) NOT NULL,
+  description VARCHAR(255),
+  quantity DECIMAL(9,3) NOT NULL,
+  invoice_date DATETIME NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  customer_id DECIMAL(9,1),
+  country VARCHAR(255),
+  PRIMARY KEY (transaction_id)
 );
--- sql/create-transaction-table-snowflake.sql
+++ sql/create-transaction-table-snowflake.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE TABLE "transactions" (
-  "transaction_id" NUMBER,
-  "invoice" VARCHAR(55),
-  "stock_code" VARCHAR(55),
-  "description" VARCHAR(255),
-  "quantity" NUMBER(9,3),
-  "invoice_date" TIMESTAMP_NTZ(9),
-  "price" NUMBER(10,2),
-  "customer_id" NUMBER(9,1),
-  "country" VARCHAR(255)
+CREATE OR REPLACE TABLE transactions (
+  transaction_id NUMBER,
+  invoice VARCHAR(55),
+  stock_code VARCHAR(55),
+  description VARCHAR(255),
+  quantity NUMBER(9,3),
+  invoice_date TIMESTAMP_NTZ(9),
+  price NUMBER(10,2),
+  customer_id NUMBER(9,1),
+  country VARCHAR(255)
 );
