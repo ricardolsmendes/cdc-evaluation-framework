ff27e9019eccea4666817ca1b3c5542f0504d1f4
Ricardo Mendes <ricardolsmendes@gmail.com>
2022-04-12 00:38:42 -0300

    feat: enable random invoices ingestion

--- main.py
+++ main.py
@@ -34,47 +34,36 @@ class CSVFilesReader:
 
     @classmethod
     def read_transactions(cls, file: str) -> DataFrame:
+        logging.info('')
         logging.info('Reading the transactions file...')
         df = pd.read_csv(file)
         logging.info('DONE!')
 
-        print()
-        print(df.head())
-        print()
-
-        logging.info('  > Dataframe shape: %s', df.shape)
-        logging.info('  > Dataframe index: %s', df.index)
-        logging.info('  > Dataframe columns: %s', df.columns)
-        logging.info('==================================================')
+        PandasHelper.print_df_metadata(df)
 
         return df
 
 
 """
-Database table writer
+Database table manager
 ========================================
 """
 
 
-class DBTableWriter:
+class DBTablesManager:
 
     def __init__(self, db_conn_string: str):
         self.__db_conn_string = db_conn_string
 
-    def write_invoices(self, transactions: DataFrame,
-                       write_delay: float) -> None:
-
-        logging.info('')
-        logging.info('Looking for unique invoice numbers...')
-        invoice_numbers = transactions['Invoice'].unique()
-        logging.info('  > %d found', len(invoice_numbers))
+    def insert_invoices(self, transactions: DataFrame,
+                        write_delay: float) -> None:
 
         logging.info('')
         logging.info('Connecting to the database...')
         con = self.create_db_connection()
 
         logging.info('')
-        logging.info('Ingesting invoices...')
+        logging.info('Inserting invoices...')
 
         db_like_df = transactions.rename(
             columns={
@@ -88,10 +77,10 @@ class DBTableWriter:
                 'Country': 'country'
             })
 
-        for invoice in invoice_numbers:
+        for invoice in transactions['Invoice'].unique():
             logging.info('')
             invoice_items = db_like_df[db_like_df['invoice'] == invoice]
-            logging.info('  Ingesting invoice "%s" with %d items...', invoice,
+            logging.info('  Inserting invoice "%s" with %d items...', invoice,
                          len(invoice_items))
 
             print()
@@ -116,6 +105,75 @@ class DBTableWriter:
 
 
 """
+Pandas helper
+========================================
+"""
+
+
+class PandasHelper:
+
+    @classmethod
+    def print_df_metadata(cls, df: DataFrame) -> None:
+        print()
+        logging.info('  > Dataframe shape: %s', df.shape)
+        logging.info('  > Dataframe index: %s', df.index)
+        logging.info('  > Dataframe columns: %s', df.columns)
+
+        print()
+        print('  > Dataframe head:')
+        print(df.head())
+        print()
+
+        logging.info('==================================================')
+
+    @classmethod
+    def select_random_subsets(cls, df: DataFrame, id_column: str,
+                              n: int) -> DataFrame:
+
+        logging.info('')
+        logging.info('Selecting %d random subsets...', n)
+        unique_subset_ids = cls.select_unique_values(df, id_column)
+        random_subset_ids = cls.select_random_items(unique_subset_ids, n)
+
+        random_subsets = pd.DataFrame()
+
+        for _, row in random_subset_ids.iterrows():
+            subset_id = row[id_column]
+            subset = df[df[id_column] == subset_id]
+            random_subsets = pd.concat([random_subsets, subset])
+
+        logging.info('DONE!')
+
+        cls.print_df_metadata(random_subsets)
+
+        return random_subsets
+
+    @classmethod
+    def select_random_items(cls, df: DataFrame, n: int) -> DataFrame:
+        logging.info('')
+        logging.info('Selecting %d random items...', n)
+        random_items = df.sample(n)
+        logging.info('DONE!')
+
+        cls.print_df_metadata(random_items)
+
+        return random_items
+
+    @classmethod
+    def select_unique_values(cls, df: DataFrame, column: str) -> DataFrame:
+        logging.info('')
+        logging.info('Selecting unique values for "%s"...', column)
+        unique_values = df[column].unique()
+        logging.info('  > %d found', len(unique_values))
+        logging.info('DONE!')
+
+        unique_values_df = pd.DataFrame(unique_values, columns=[column])
+        cls.print_df_metadata(unique_values_df)
+
+        return unique_values_df
+
+
+"""
 Main program entry point
 ========================================
 """
@@ -125,6 +183,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Ingest transactions data')
 
     parser.add_argument('--data-file', help='the CSV data file', required=True)
+    parser.add_argument('--invoices', help='the number of invoices', default=0)
     parser.add_argument('--db-conn',
                         help='the database connection string for SQLAlchemy',
                         required=True)
@@ -136,6 +195,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     transactions_df = CSVFilesReader.read_transactions(args.data_file)
-    db_table_writer = DBTableWriter(args.db_conn)
-    db_table_writer.write_invoices(transactions=transactions_df,
-                                   write_delay=float(args.write_delay))
+
+    n_invoices = int(args.invoices)
+    if n_invoices > 0:
+        transactions_df = PandasHelper.select_random_subsets(
+            transactions_df, 'Invoice', n_invoices)
+
+    db_table_writer = DBTablesManager(args.db_conn)
+    db_table_writer.insert_invoices(transactions=transactions_df,
+                                    write_delay=float(args.write_delay))
