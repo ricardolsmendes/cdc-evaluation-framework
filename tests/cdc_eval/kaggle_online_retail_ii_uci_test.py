c00b0c1912f5843cbe09fc34527bd314cbac2ece
Ricardo Mendes <ricardolsmendes@gmail.com>
2023-01-21 16:40:21 -0300

    test: add tests for insert_invoices

--- tests/cdc_eval/kaggle_online_retail_ii_uci_test.py
+++ tests/cdc_eval/kaggle_online_retail_ii_uci_test.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Ricardo Mendes
+# Copyright 2023 Ricardo Mendes
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,10 +31,10 @@ class CSVFilesReaderTest(unittest.TestCase):
 
     @mock.patch(f'{_ONLINE_RETAIL_MODULE}.pd.read_csv')
     def test_read_transactions_should_return_data_frame_on_success(self, mock_read_csv):
-        fake_data_frame = pd.DataFrame()
-        mock_read_csv.return_value = fake_data_frame
+        data_frame = pd.DataFrame()
+        mock_read_csv.return_value = data_frame
         return_value = online_retail.CSVFilesReader.read_transactions('test.csv')
-        self.assertTrue(fake_data_frame.equals(return_value))
+        self.assertTrue(data_frame.equals(return_value))
 
 
 class TransactionsDBManagerTest(unittest.TestCase):
@@ -54,7 +54,7 @@ class TransactionsDBManagerTest(unittest.TestCase):
     def test_delete_invoices_should_delete_by_unique_invoice_numbers(
             self, mock_create_db_connection, mock_get_existing_table, mock_delete):
 
-        fake_data = {
+        data = {
             'Invoice': [489434, 489434, 489435, 489436, 489436, 489437],
             'StockCode': ['85048', '79323P', '22350', '48173C', '21755', '22143'],
             'Description': [
@@ -64,10 +64,30 @@ class TransactionsDBManagerTest(unittest.TestCase):
             ],
             'Quantity': [12, 12, 12, 18, 18, 6]
         }
-        transactions = pd.DataFrame(fake_data)
-        fake_conn = mock_create_db_connection.return_value
+        transactions = pd.DataFrame(data)
+        mock_conn = mock_create_db_connection.return_value
 
         self._db_manager.delete_invoices(transactions, 0)
 
-        mock_get_existing_table.assert_called_once_with(fake_conn, 'transactions')
-        self.assertEqual(mock_delete.call_count, 4)  # One for each invoice number
+        mock_get_existing_table.assert_called_once_with(mock_conn, 'transactions')
+        self.assertEqual(mock_delete.call_count, 4)  # One call for each invoice number
+
+    @mock.patch('pandas.io.sql.to_sql')
+    @mock.patch(f'{_DB_MANAGER_CLASS}.create_db_connection', lambda *args: None)
+    def test_insert_invoices_should_insert_item_batches_grouped_by_invoice(
+            self, mock_to_sql):
+
+        data = {
+            'Invoice': [489434, 489434, 489435, 489436, 489436, 489437],
+            'StockCode': ['85048', '79323P', '22350', '48173C', '21755', '22143'],
+            'Description': [
+                '15CM CHRISTMAS GLASS BALL 20 LIGHTS', 'PINK CHERRY LIGHTS', 'CAT BOWL',
+                'DOOR MAT BLACK FLOCK', 'LOVE BUILDING BLOCK WORD',
+                'CHRISTMAS CRAFT HEART DECORATIONS'
+            ],
+            'Quantity': [12, 12, 12, 18, 18, 6]
+        }
+        transactions = pd.DataFrame(data)
+
+        self._db_manager.insert_invoices(transactions, 0)
+        self.assertEqual(mock_to_sql.call_count, 4)  # One call for each invoice
